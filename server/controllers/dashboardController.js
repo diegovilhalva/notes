6d1d9bb92690067b2e844b0555fb49967825477a
Diego Vilhlava <diegorocha450@yahoo.com>
2024-06-18 12:51:44 -0300

    pagination added to the dashboard

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -4,19 +4,40 @@ import Note from "../models/Notes.js"
 
 export const dashboard = async (req, res) => {
 
+    let perPage = 12
+    let page = req.query.page || 1
     const locals = {
         title: 'Painel de controle',
         description: 'Crie suas notas grátis'
     }
 
     try {
-        const notes = await Note.find({})
+        const notes = await Note.aggregate([
+            { $sort: { updatedAt: -1 } },
+            { $match: { user: new mongoose.Types.ObjectId(req.user.id) } },
+            {
+                $project: {
+                    title: { $substr: ["$title", 0, 30] },
+                    body: { $substr: ["$body", 0, 100] },
+                },
+            }
+        ])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+        const count = await Note.countDocuments({user:new mongoose.Types.ObjectId(req.user.id)});
+       
         res.render('dashboard/index', {
             userName: req.user.firstName,
             locals,
             notes,
-            layout: '../views/layouts/dashboard'
-        })
+            layout: "../views/layouts/dashboard",
+            current: page,
+            pages: Math.ceil(count / perPage)
+        });
+
+
     } catch (error) {
         console.log(error)
     }
