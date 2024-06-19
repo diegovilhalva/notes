201f18647b6480b87bdd069a2307ba89c1809fa1
Diego Vilhlava <diegorocha450@yahoo.com>
2024-06-19 11:53:23 -0300

    added more pages and styles to the project

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -17,3 +17,19 @@ export const about = async (req,res) => {
     }
     res.render('about',locals)
 }
+
+export const features = async (req,res) => {
+    const locals  = {
+        title:'Notes - Recursos ',
+        description:'Crie suas notas grátis'
+    }
+    res.render('features',locals)
+}
+
+export const faq = async (req,res) => {
+    const locals  = {
+        title:'Notes - Faq ',
+        description:'Crie suas notas grátis'
+    }
+    res.render('faq',locals)
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { about, homePage } from "../controllers/mainController.js"
+import { about, faq, features, homePage } from "../controllers/mainController.js"
 
 const router = express.Router()
 
 router.get('/',homePage)
 router.get('/about',about)
+router.get('/features',features)
+router.get('/faq',faq)
 
 export default router
